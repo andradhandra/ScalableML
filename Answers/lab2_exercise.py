97bd7ec17c5ce33fb038dce664965cd520f52240
Ridza Adhandra <andradhandra@gmail.com>
2024-02-20 01:10:51 +0000

    Answer fist part of Lab 2 Exercise

--- Answers/lab2_exercise.py
+++ Answers/lab2_exercise.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 
 spark = SparkSession.builder \
     .master("local[4]") \
@@ -9,12 +10,41 @@ spark = SparkSession.builder \
 sc = spark.sparkContext
 sc.setLogLevel("WARN")  # This can only affect the log level after it is executed.
 
-logFile=spark.read.text("./Data/NASA_access_log_Aug95.gz").cache()
+### Start code
 
-### Start mining code
+## Log mininng NASA data
+# Read log data as text file and processing the columns to match requirement
+log_df = spark.read.text("./Data/NASA_access_log_Aug95.gz").cache() \
+          .withColumn("fields", F.split(F.col("value"), " ")) \
+          .withColumn("host", F.col("fields").getItem(0)) \
+          .withColumn("timestamp", F.concat(F.col("fields").getItem(3), F.col("fields").getItem(4))) \
+          .withColumn("request", F.concat(F.col("fields").getItem(5), F.col("fields").getItem(6), F.col("fields").getItem(7))) \
+          .withColumn("response_code", F.col("fields").getItem(8)) \
+          .withColumn("bit_data", F.col("fields").getItem(9))
 
+df = log_df.drop(*["value", "fields"]).cache()
 
+# Print DataFrame
+print()
+print("Nasa Log File DataFrame")
+print("=======================")
+df.show(10, False) 
+df.printSchema()
 
-### End mining code
+# Count unique data in dataframe
+unique = df.distinct().count()
+print("\n\nHello Spark: There are %i unique data from the log file.\n" % (unique))
+
+# Find the most frequent visitor
+grouped = df.groupby("host").count()
+
+print("Number of host visit")
+print("====================")
+grouped.orderBy(F.desc('count')).show(truncate = False)
+
+frequent_host = grouped.orderBy(F.desc('count')).first()
+print("\n\nHello Spark: The most frequent visited host are {} with {} visits.\n".format(frequent_host['host'], frequent_host['count']))
+
+### End code
 
 spark.stop()

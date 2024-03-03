6f198848dc84d1b9d6b91b6780fdcf68523a8fa6
Ridza Adhandra <andradhandra@gmail.com>
2024-03-03 22:20:40 +0000

    add result for lab4

--- Answers/lab4_exercise.py
+++ Answers/lab4_exercise.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import DoubleType
-from pyspark.ml.regression import GeneralizedLinearRegression
+from pyspark.ml.regression import GeneralizedLinearRegression, LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler, OneHotEncoder
@@ -46,21 +46,112 @@ encoded_rawdata = encoded_model.transform(new_rawdata)
 # Assemble the features into a vector
 assembler = VectorAssembler(inputCols = encoded_schemaNames+new_schemaNames[8:12], outputCol = 'features') 
 
-# Apply Poisson Regression
+# Create Poisson Regression Instance
 glm_poisson = GeneralizedLinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.01, family='poisson', link='log')
 
-# Create a pipeline
+# Create Linear Regression with l1 regularisation
+lr1_auto = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=1.0, solver="auto")
+lr1_normal = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=1.0, solver="normal")
+
+# Create Linear Regression with l2 regularisation
+lr2_auto = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=0.0, solver="auto")
+lr2_normal = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=0.0, solver="normal")
+lr2_lbfgs = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=0.0, solver="l-bfgs")
+
+# Create Linear Regression with elastic net regularisation
+en_auto = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=0.5, solver="auto")
+en_normal = LinearRegression(featuresCol='features', labelCol='cnt', maxIter=50, regParam=0.1, elasticNetParam=0.5, solver="normal")
+
+
+# Create poisson pipeline and calculate RMSE
 stages = [assembler, glm_poisson]
 pipeline = Pipeline(stages=stages)
 
-# Fit pipeline with the dataset
 pipelineModel = pipeline.fit(trainingData)
 
-# Calculate RMSE
 predictions = pipelineModel.transform(testData)
 evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
 rmse = evaluator.evaluate(predictions)
 print("RMSE of GLM poisson model ===> %g " % rmse)
+
+# Create lr1 auto pipeline and calculate RMSE
+stages = [assembler, lr1_auto]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE of lr1 auto ===> %g " % rmse)
+
+# Create lr1 normal and alculate RMSE
+stages = [assembler, lr1_normal]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE of lr1 normal ===> %g " % rmse)
+
+# Create lr2 auto and alculate RMSE
+stages = [assembler, lr2_auto]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE of lr2 auto ===> %g " % rmse)
+
+# Create lr2 normal and alculate RMSE
+stages = [assembler, lr2_normal]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+
+print("RMSE of lr2 normal ===> %g " % rmse)
+
+# Create lr2 l-bfgs and alculate RMSE
+stages = [assembler, lr2_lbfgs]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE of lr2 l-bfgs ===> %g " % rmse)
+
+# Create elastic net auto and alculate RMSE
+stages = [assembler, en_auto]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE of elastic net auto ===> %g " % rmse)
+
+# Create elastic net normal and alculate RMSE
+stages = [assembler, en_normal]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(trainingData)
+
+predictions = pipelineModel.transform(testData)
+evaluator = RegressionEvaluator(labelCol="cnt", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE of elastic net normal ===> %g " % rmse)
+
 
 # End code
 
